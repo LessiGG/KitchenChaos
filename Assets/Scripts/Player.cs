a3d154cf870ee6d244088dae0b65ff2a3b68923a
LessiGG <ivan-lesnichenko@mail.ru>
2024-04-24 17:20:59 +0300

    Player animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 7f;
+    [SerializeField] private float _rotateSpeed = 10f;
+
+    private bool _isWalking;
 
     private void Update()
     {
@@ -29,6 +32,14 @@ public class Player : MonoBehaviour
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
+        _isWalking = moveDirection != Vector3.zero;
+
         transform.position += moveDirection * _moveSpeed * Time.deltaTime;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, _rotateSpeed * Time.deltaTime);
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
     }
 }
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+
+    private Animator _animator;
+
+    private const string IsWalking = "IsWalking";
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _animator.SetBool(IsWalking, _player.IsWalking());
+    }
+}
