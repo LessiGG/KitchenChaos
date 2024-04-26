096f09b28d85844601f188d1e884ba7f5f6577a7
LessiGG <ivan-lesnichenko@mail.ru>
2024-04-26 16:59:40 +0300

    Added kitchen object parent
    Added kitchen object parent in order to know when our clear counter is occupied with an item

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,12 +5,43 @@ public class ClearCounter : MonoBehaviour
     [SerializeField] private KitchenObjectSO _prefab;
     [SerializeField] private Transform _counterTopPoint;
 
+    private KitchenObject _kitchenObject;
+
     public void Interact()
     {
-        Debug.Log("Interact!");
-        Transform kitchenObjectTransform = Instantiate(_prefab.Prefab, _counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if (_kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(_prefab.Prefab, _counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(_kitchenObject.GetClearCounter());
+        }
+    }
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().ObjectName);
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return _counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        _kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return _kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        _kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return _kitchenObject != null;
     }
 }
