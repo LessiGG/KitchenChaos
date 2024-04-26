428ed4107ec6fa130059e3725394dc2e9c35b29c
LessiGG <ivan-lesnichenko@mail.ru>
2024-04-26 19:17:28 +0300

    Pick up and drop down objects, added all possible containers

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,5 +6,32 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
+        if (!HasKitchenObject())
+        {
+            // There is no kitchenObject here
+
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Player is not carrying anything
+            }
+        }
+        else
+        {
+            // There is a KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+            }
+            else
+            {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
