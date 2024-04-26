5b5504f78862261a58c95d57f1af7fd84e3f1609
LessiGG <ivan-lesnichenko@mail.ru>
2024-04-26 16:35:01 +0300

    Added kitchen objects as scriptable objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO _prefab;
+    [SerializeField] private Transform _counterTopPoint;
+
     public void Interact()
     {
         Debug.Log("Interact!");
+        Transform kitchenObjectTransform = Instantiate(_prefab.Prefab, _counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().ObjectName);
     }
 }
